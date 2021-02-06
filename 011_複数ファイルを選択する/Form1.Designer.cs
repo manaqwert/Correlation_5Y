d4d6a800c89e6ed2e737f9e61360f971f52f998b
manaqwert <78248651+manaqwert@users.noreply.github.com>
2021-02-06 23:23:12 +0900

    プロジェクト ファイルを追加します。

---
+++ 011_複数ファイルを選択する/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace _011_複数ファイルを選択する
+{
+    partial class 複数ファイルを選択する
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File1_sel_Box = new System.Windows.Forms.TextBox();
+            this.File1_sel_button = new System.Windows.Forms.Button();
+            this.File2_sel_Box = new System.Windows.Forms.TextBox();
+            this.File2_sel_button = new System.Windows.Forms.Button();
+            this.File3_sel_Box = new System.Windows.Forms.TextBox();
+            this.File3_sel_button = new System.Windows.Forms.Button();
+            this.File4_sel_Box = new System.Windows.Forms.TextBox();
+            this.File4_sel_button = new System.Windows.Forms.Button();
+            this.File5_sel_Box = new System.Windows.Forms.TextBox();
+            this.File5_sel_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Compute_button = new System.Windows.Forms.Button();
+            this.Quit_button = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // File1_sel_Box
+            // 
+            this.File1_sel_Box.Location = new System.Drawing.Point(113, 17);
+            this.File1_sel_Box.Name = "File1_sel_Box";
+            this.File1_sel_Box.Size = new System.Drawing.Size(773, 19);
+            this.File1_sel_Box.TabIndex = 5;
+            this.File1_sel_Box.TextChanged += new System.EventHandler(this.File1_sel_Box_TextChanged);
+            // 
+            // File1_sel_button
+            // 
+            this.File1_sel_button.Location = new System.Drawing.Point(902, 17);
+            this.File1_sel_button.Name = "File1_sel_button";
+            this.File1_sel_button.Size = new System.Drawing.Size(92, 29);
+            this.File1_sel_button.TabIndex = 6;
+            this.File1_sel_button.Text = "Select File";
+            this.File1_sel_button.UseVisualStyleBackColor = true;
+            this.File1_sel_button.Click += new System.EventHandler(this.File1_sel_button_Click);
+            // 
+            // File2_sel_Box
+            // 
+            this.File2_sel_Box.Location = new System.Drawing.Point(113, 52);
+            this.File2_sel_Box.Name = "File2_sel_Box";
+            this.File2_sel_Box.Size = new System.Drawing.Size(773, 19);
+            this.File2_sel_Box.TabIndex = 7;
+            this.File2_sel_Box.TextChanged += new System.EventHandler(this.File2_sel_Box_TextChanged);
+            // 
+            // File2_sel_button
+            // 
+            this.File2_sel_button.Location = new System.Drawing.Point(902, 52);
+            this.File2_sel_button.Name = "File2_sel_button";
+            this.File2_sel_button.Size = new System.Drawing.Size(92, 28);
+            this.File2_sel_button.TabIndex = 8;
+            this.File2_sel_button.Text = "Select File";
+            this.File2_sel_button.UseVisualStyleBackColor = true;
+            this.File2_sel_button.Click += new System.EventHandler(this.File2_sel_button_Click);
+            // 
+            // File3_sel_Box
+            // 
+            this.File3_sel_Box.Location = new System.Drawing.Point(113, 87);
+            this.File3_sel_Box.Name = "File3_sel_Box";
+            this.File3_sel_Box.Size = new System.Drawing.Size(773, 19);
+            this.File3_sel_Box.TabIndex = 9;
+            this.File3_sel_Box.TextChanged += new System.EventHandler(this.File3_sel_Box_TextChanged);
+            // 
+            // File3_sel_button
+            // 
+            this.File3_sel_button.Location = new System.Drawing.Point(902, 87);
+            this.File3_sel_button.Name = "File3_sel_button";
+            this.File3_sel_button.Size = new System.Drawing.Size(92, 29);
+            this.File3_sel_button.TabIndex = 10;
+            this.File3_sel_button.Text = "Select File";
+            this.File3_sel_button.UseVisualStyleBackColor = true;
+            this.File3_sel_button.Click += new System.EventHandler(this.File3_sel_button_Click);
+            // 
+            // File4_sel_Box
+            // 
+            this.File4_sel_Box.Location = new System.Drawing.Point(113, 122);
+            this.File4_sel_Box.Name = "File4_sel_Box";
+            this.File4_sel_Box.Size = new System.Drawing.Size(773, 19);
+            this.File4_sel_Box.TabIndex = 11;
+            this.File4_sel_Box.TextChanged += new System.EventHandler(this.File4_sel_Box_TextChanged);
+            // 
+            // File4_sel_button
+            // 
+            this.File4_sel_button.Location = new System.Drawing.Point(902, 122);
+            this.File4_sel_button.Name = "File4_sel_button";
+            this.File4_sel_button.Size = new System.Drawing.Size(91, 29);
+            this.File4_sel_button.TabIndex = 12;
+            this.File4_sel_button.Text = "Select File";
+            this.File4_sel_button.UseVisualStyleBackColor = true;
+            this.File4_sel_button.Click += new System.EventHandler(this.File4_sel_button_Click);
+            // 
+            // File5_sel_Box
+            // 
+            this.File5_sel_Box.Location = new System.Drawing.Point(113, 158);
+            this.File5_sel_Box.Name = "File5_sel_Box";
+            this.File5_sel_Box.Size = new System.Drawing.Size(773, 19);
+            this.File5_sel_Box.TabIndex = 13;
+            this.File5_sel_Box.TextChanged += new System.EventHandler(this.File5_sel_Box_TextChanged);
+            // 
+            // File5_sel_button
+            // 
+            this.File5_sel_button.Location = new System.Drawing.Point(902, 157);
+            this.File5_sel_button.Name = "File5_sel_button";
+            this.File5_sel_button.Size = new System.Drawing.Size(90, 29);
+            this.File5_sel_button.TabIndex = 14;
+            this.File5_sel_button.Text = "Select File";
+            this.File5_sel_button.UseVisualStyleBackColor = true;
+            this.File5_sel_button.Click += new System.EventHandler(this.File5_sel_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ファイル１：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "ファイル２：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "ファイル３：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "ファイル４：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "ファイル５：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(16, 204);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(978, 256);
+            this.listBox1.TabIndex = 20;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Compute_button
+            // 
+            this.Compute_button.Location = new System.Drawing.Point(787, 569);
+            this.Compute_button.Name = "Compute_button";
+            this.Compute_button.Size = new System.Drawing.Size(104, 26);
+            this.Compute_button.TabIndex = 21;
+            this.Compute_button.Text = "Compute";
+            this.Compute_button.UseVisualStyleBackColor = true;
+            this.Compute_button.Click += new System.EventHandler(this.Compute_button_Click);
+            // 
+            // Quit_button
+            // 
+            this.Quit_button.Location = new System.Drawing.Point(902, 569);
+            this.Quit_button.Name = "Quit_button";
+            this.Quit_button.Size = new System.Drawing.Size(94, 26);
+            this.Quit_button.TabIndex = 22;
+            this.Quit_button.Text = "Quit";
+            this.Quit_button.UseVisualStyleBackColor = true;
+            this.Quit_button.Click += new System.EventHandler(this.Quit_button_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 485);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 12);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "騰落率閾値：";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(91, 482);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(127, 19);
+            this.textBox10.TabIndex = 24;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(265, 485);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 12);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "標準偏差閾値：";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(354, 482);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(126, 19);
+            this.textBox11.TabIndex = 26;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 521);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(976, 25);
+            this.progressBar1.TabIndex = 27;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // 複数ファイルを選択する
+            // 
+            this.ClientSize = new System.Drawing.Size(1022, 615);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Quit_button);
+            this.Controls.Add(this.Compute_button);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.File5_sel_button);
+            this.Controls.Add(this.File5_sel_Box);
+            this.Controls.Add(this.File4_sel_button);
+            this.Controls.Add(this.File4_sel_Box);
+            this.Controls.Add(this.File3_sel_button);
+            this.Controls.Add(this.File3_sel_Box);
+            this.Controls.Add(this.File2_sel_button);
+            this.Controls.Add(this.File2_sel_Box);
+            this.Controls.Add(this.File1_sel_button);
+            this.Controls.Add(this.File1_sel_Box);
+            this.Name = "複数ファイルを選択する";
+            this.Load += new System.EventHandler(this.複数ファイルを選択する_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.TextBox ファイル１;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox File1_sel_Box;
+        private System.Windows.Forms.Button File1_sel_button;
+        private System.Windows.Forms.TextBox File2_sel_Box;
+        private System.Windows.Forms.Button File2_sel_button;
+        private System.Windows.Forms.TextBox File3_sel_Box;
+        private System.Windows.Forms.Button File3_sel_button;
+        private System.Windows.Forms.TextBox File4_sel_Box;
+        private System.Windows.Forms.Button File4_sel_button;
+        private System.Windows.Forms.TextBox File5_sel_Box;
+        private System.Windows.Forms.Button File5_sel_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Compute_button;
+        private System.Windows.Forms.Button Quit_button;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
